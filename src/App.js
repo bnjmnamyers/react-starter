f8ea56e2999631fc2a6e7fede0a668cc6e38902f
Ben Myers <ben.myers@benjaminmyers.com>
2019-05-06 10:58:13 -0400

    Removed reference to non-existent component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
-import Greeting from './containers/Greeting/Greeting';
 import './styles/main.scss';
 
 class App extends Component {
     render() {
         return (
-            <h1>Hello, React!</h1>
+            <div className="App">
+                <h1>Hello, React!</h1>
+            </div>
         );
     }
 }
